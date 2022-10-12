e4c435f8084296bd14e2fc21cad304ab82d478e5
Chris Chen <mailchristianchen@gmail.com>
2022-10-12 22:17:02 +0100

    test deployment

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flaskr import create_app
 
 app = create_app()
 
-# if __name__ == "__main__":
-#     from waitress import serve
-#     serve(app, host="0.0.0.0", port=8080)
-
 if __name__ == "__main__":
-    app.run(port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0")
+
+# if __name__ == "__main__":
+#     app.run(port=8080)
